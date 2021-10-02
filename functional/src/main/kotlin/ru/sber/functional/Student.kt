de69988d8923ac19a18f10e50023c49e8b1fb7a5
Vincent Marillion <vmarilli@student.21-school.ru>
2021-10-02 16:03:38 +0300

    functional исправление

--- functional/src/main/kotlin/ru/sber/functional/Student.kt
+++ functional/src/main/kotlin/ru/sber/functional/Student.kt
@@ -3,10 +3,10 @@ package ru.sber.functional
 data class Student(
     val firstName: String,
     val lastName: String,
-    val middleName: String,
-    val age: Int,
+    val middleName: String = "none",
+    val age: Int = 50,
     val averageRate: Double,
-    val city: String,
-    val specialization: String,
-    val prevEducation: String?,
+    val city: String = "none",
+    val specialization: String = "none",
+    val prevEducation: String? = "none"
 )
--- functional/src/test/java/ru/sber/functional/StudentTest.kt
+++ functional/src/test/java/ru/sber/functional/StudentTest.kt
@@ -9,10 +9,10 @@ class StudentTest {
 
     @Test
     fun filterTest(){
-        val student1 = Student("Petr", "Petrov", "none", 20, 4.2, "none", "none", "none")
-        val student2 = Student("Ivan", "Petrov", "none", 19, 4.4, "none", "none", "none")
-        val student3 = Student("Dmitriy", "Petrov", "none", 21, 4.7, "none", "none", "none")
-        val student4 = Student("Petr", "Ivanov", "none", 22, 4.5, "none", "none", "none")
+        val student1 = Student(firstName = "Petr", lastName = "Petrov",  averageRate = 4.2)
+        val student2 = Student(firstName = "Ivan", lastName = "Petrov",  averageRate = 4.4)
+        val student3 = Student(firstName = "Dmitriy", lastName = "Petrov",  averageRate = 4.7)
+        val student4 = Student(firstName = "Petr", lastName = "Ivanov", averageRate =  4.5)
         studentsGroup.students = listOf(student1, student2, student3, student4)
         val studentsTest = listOf(student1)
         assertEquals(studentsGroup.filterByPredicate { it.averageRate < 4.4 }, studentsTest)
